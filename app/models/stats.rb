7fbfd1cb1e693c2bfc94337a2dd177299c51f35a
pete gamache <pete@gamache.org>
2012-11-21 16:43:50 -0500

    shit be WORKIN with stats

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -6,10 +6,20 @@ class Stats < Hash
   #
   ## It's a plain hash that looks like this at the top level:
   #
-  # { :season => {
+  # { :overall => {
   #     :career => <stats hash>,
-  #     1999  => <stats hash>,
-  #     2000  => <stats hash>,
+  #     1999    => <stats hash>,
+  #     2000    => <stats hash>,
+  #     ...
+  #   },
+  #   :season => {
+  #     :career => {
+  #       'player1' => <stats hash>,
+  #       'player2' => <stats hash>,
+  #       ...
+  #     },
+  #     1999  => { ... },
+  #     2000  => { ... },
   #     ...
   #   },
   #   :player => {
@@ -35,14 +45,13 @@ class Stats < Hash
   #   }
   # }
   #
-  ## Five types of statistic are being kept:
-  #
-  # stats[:season][:career] is the overall tally for all games.
-  # stats[:season][1999] is the overall tally for the 1999 season.
-  # stats[:player]['triode'][:career] is triode's career stats.
-  # stats[:player]['triode'][1999] is triode's 1999 stats.
-  # stats[:night][2012][1]['triode'] is triode's stats for opening
-  #   night of the 2012 season.
+  # stats[:overall][:career] is the overall sum of all November Games records.
+  # stats[:overall][1999] is the overall sum of November Games 1999.
+  # stats[:season][:career] is the career MAAG, sorted by warps (descending).
+  # stats[:season][2010] is the November Games 2010 MAAG.
+  # stats[:player]['triode'][:career] is Triode's career totals.
+  # stats[:player]['77k'][1999] is 77K's 1999 totals.
+  # stats[:night][2012][1]['triode'] is triode's stats for opening night 2012.
 
   COLLECTED_STATS = [:warps, :games, :games_won, :cfbs, :come_ons,
                      :wimp_marks, :mystery_factors]
@@ -51,6 +60,7 @@ class Stats < Hash
 
   def initialize(set={})
     self.merge!({
+      :overall => {},
       :season => {},
       :player => {},
       :night => {}
@@ -70,7 +80,6 @@ private
     ingest_fudges!(fudges)
 
     compute_stats!
-    expose_failures ## FIXME omg dubious thinking ahead
 
     sort_seasons_by_warps!
     mark_top_winners!
@@ -80,7 +89,8 @@ private
 
   ## For a given player name, season, and night, returns a list of
   ## stats hashes to which a Score or Fudge should be added.
-  ## Any or all of the player name, season, and night may be nil.
+  ## Any or all of the player name, season, and night may be nil,
+  ## which means the number of stats hashes returned may vary.
   def get_stats_hashes(attrs)
     player_name = attrs[:name]
     season = attrs[:season]
@@ -88,32 +98,37 @@ private
 
     stats_hashes = []
 
-    ## set up season career (overall) record
+    ## career record for this player
     self[:season][:career] ||= {}
     stats_hashes << self[:season][:career][player_name] ||= {}
 
-    ## set up night record
+    ## season record for this player
+    if season
+      self[:season][season] ||= {}
+      stats_hashes << self[:season][season][player_name] ||= {}
+    end
+
+    ## night record for this player
     if night && player_name
       night_by_season = self[:night][season] ||= {}
       night_by_player = night_by_season[night] ||= {}
       stats_hashes << night_by_player[player_name] ||= {}
     end
 
-    ## set up season record
-    if season
-      self[:season][season] ||= {}
-      stats_hashes << self[:season][season][player_name] ||= {}
-    end
-
-    ## set up player career and player season records
+    ## player career and player season records
     if player_name
       p = self[:player][player_name] ||= {}
-      stats_hashes << p[season] ||= {} if season
       stats_hashes << p[:career] ||= {}
+      stats_hashes << p[season]  ||= {} if season
     end
+
+    ## overall career and season records
+    stats_hashes << self[:overall][:career] ||= {}
+    stats_hashes << self[:overall][season]  ||= {} if season
 
     stats_hashes
   end
+
 
   ## Adds non-computed information about each Score to the stats.
   def ingest_scores!(scores)
@@ -136,32 +151,24 @@ private
 
   end
 
-  def expose_failures
-    self[:season].each do |season, stats|
-      stats.each do |player, stats_hash|
-        pp stats_hash unless stats_hash.is_a? Hash
-      end
-    end
-  end
 
   ## Adds non-computed information about each Fudge to the stats.
   def ingest_fudges!(fudges)
     fudges.each do |fudge|
       stats_hashes = get_stats_hashes(:name => fudge.player.name,
                                       :season => fudge.season)
-      COLLECTED_STATS.each do |stat|
+      STATS.each do |stat|
         value = fudge.send(stat)
         stats_hashes.each do |stats_hash|
           stats_hash[stat] ||= 0
           stats_hash[stat] += value.to_i
         end
       end
-
     end
   end
 
+
   ## Adds computed information to the stats.
-  ## FIXME something is wrong here
   def compute_stats!
     ## calculate nights, nights won, high night, and gold stars by using the
     ## self[:night] structure.
@@ -178,10 +185,10 @@ private
 
       nights.each do |night|
         ## increment overall per-season and career total
-        self[:season][:career][:nights] ||= 0
-        self[:season][:career][:nights] += 1
-        self[:season][season][:nights] ||= 0
-        self[:season][season][:nights] += 1
+        self[:overall][:career][:nights] ||= 0
+        self[:overall][:career][:nights] += 1
+        self[:overall][season][:nights] ||= 0
+        self[:overall][season][:nights] += 1
 
         ## used for nights_won
         most_warps = 0
@@ -237,15 +244,22 @@ private
     end
   end
 
-  ## FIXME this part is totally fucked
+
   def sort_seasons_by_warps!
     self[:season].each do |season, stats|
-      # stats.each {|player, st| pp season unless st.is_a? Hash}
-      self[:season][season] = Hash[ stats.sort_by {|k,v| 
-        -v[:warps] rescue 0
+      stats.each {|player, st| 
+        unless st.is_a? Hash
+          pp player
+          pp st
+          pp season
+        end
+      }
+      self[:season][season] = Hash[ stats.sort_by {|k,v|
+        -v[:warps].to_i
       }]
     end
   end
+
 
   ## Mark the winners of each stat in the top N stats (by warps).
   def mark_top_winners!(n=50)
@@ -255,9 +269,10 @@ private
       top = Hash[ stats_hashes.to_a[0..n] ]
       STATS.each do |stat|
         score = nil
-        top.sort_by{|k,v| -v[stat]}.each do |player, stats_hash|
+        top.sort_by{|k,v| -v[stat].to_f}.each do |player, stats_hash|
           score ||= stats_hash[stat]
           break unless score == stats_hash[stat]
+          break unless score.to_f > 0 # don't mark scores of 0 as winning shit
           stats_hash[:top_scores] ||= []
           stats_hash[:top_scores] << stat
         end
@@ -267,252 +282,3 @@ private
 
 end
 
-__END__
-
-
-  ## Stats.new returns a Stats object (hash), with sane default values.
-  class << self; alias_method :orig_new, :new end
-  def self.new
-    self.orig_new do |h,k|
-      h[k] = Hash[:warps => 0,
-                  :games => 0,
-                  :nights => 0,
-                  :nights_won => 0,
-                  :high_night => 0,
-                  :wins => 0,
-                  :cfbs => 0,
-                  :come_ons => 0,
-                  :wimps => 0,
-                  :mystery_factors => 0,
-                  :gold_stars => 0,
-
-                  ## :dates holds the number of warps per date
-                  ## and :nights gets computed from it
-                  :dates => Hash.new(0),
-
-                  ## :winner holds info about what this player won.
-                  ## keyed by stat, value is true or false
-                  :winner => Hash.new(false)
-      ]
-    end
-  end
-
-  ## Stats.career returns a stats hash encompassing all seasons
-  ## Results are cached (as Hash, to avoid marshaling issues).
-  def self.career
-    stats_hash = Rails.cache.fetch('stats_overall') do
-      season_stats = Season.all.map {|s| for_season(s)}
-      fudged_stats = Stats.new.apply_fudges!(Fudge.where(:season => nil).all)
-      stats = season_stats + [fudged_stats]
-      overall_stats = merge_stats(stats)
-      Hash[overall_stats]
-    end
-    Stats[stats_hash]
-  end
-
-  ## Stats.all returns a hash of Stats records, one :career as above and
-  ## the rest keyed by season year.
-  def self.all
-    Season.all.
-      inject({}) {|h,s| h.merge(s.year => for_season(s))}.
-      merge(:overall => career)
-  end
-
-  ## Stats.for_season(season) returns a stats hash for a single season.
-  ## season may be an integer or a Season object.
-  ## Results are cached (as Hash, to avoid marshaling issues).
-  def self.for_season(season)
-    season = Season.new(:year => season) unless season.is_a?(Season)
-
-    stats_hash = Rails.cache.fetch("stats#{season.year}") do
-      stats = Stats.new
-      scores = Score.where(:season => season.name).all
-      fudges = Fudge.where(:season => season.name).all
-
-      scores.each do |score|
-        h = stats[score.player_id]
-        h[:warps] += score.warps
-        h[:wimps] += score.wimps
-        h[:games] += 1
-        h[:dates][score.date] += score.warps
-        h[:wins]            += score.win            ? 1 : 0
-        h[:cfbs]            += score.cfb            ? 1 : 0
-        h[:come_ons]        += score.come_on        ? 1 : 0
-        h[:mystery_factors] += score.mystery_factor ? 1 : 0
-      end
-
-      Hash[stats.
-             apply_fudges!(fudges).
-             calculate!.
-             convert_player_ids_to_names!]
-    end
-
-    Stats[stats_hash]
-  end
-
-  def overall; self[:overall] end
-
-
-
-  ## merge_stats merges multiple Stats objects.
-  def self.merge_stats(*stats_list)
-    ## make splat notation optional
-    stats_list = stats_list.first if stats_list.first.is_a?(Array)
-
-    ## accumulate stats
-    merged_stats = Stats.new
-    stats_list.each do |stats|
-      stats.each do |player_id, stat_hash|
-        [:warps, :games, :nights, :wins, :cfbs,
-         :come_ons, :wimps, :mystery_factors, :gold_stars].each do |k|
-          merged_stats[player_id][k] += stat_hash[k]
-        end
-
-        stat_hash[:dates].each do |date,warps|
-          merged_stats[player_id][:dates][date] += warps
-        end
-
-        ## the :nights field gets destroyed during calculate.
-        ## we store it as :nights_real so we can reinstate it
-        merged_stats[player_id][:nights_real] =
-          merged_stats[player_id][:nights]
-      end
-    end
-
-    merged_stats.calculate!
-
-    ## now restore the real night counts
-    merged_stats.each do |player, stat_hash|
-      #next if player == :overall
-      stat_hash[:nights] = stat_hash.delete(:nights_real)
-    end
-
-    merged_stats
-  end
-
-  ## apply_fudges! incorporates the given fudge values into stats.
-  def apply_fudges!(*fudges)
-    ## make splat notation optional
-    fudges = fudges.first if fudges.first.is_a?(Array)
-
-    fudges.each do |f|
-      player_id = f.player_id || :overall
-      [:warps, :games, :nights, :wins, :cfbs, :come_ons,
-       :wimps, :mystery_factors, :gold_stars].each do |field|
-        self[player_id][field] += f.send(field).to_i
-       end
-      self[player_id][:high_night] = f.high_night if
-        self[player_id][:high_night].to_i < f.high_night.to_i
-    end
-
-    self
-  end
-
-  ## calculate! computes per-player and overall computed stats.
-  def calculate!
-    ov = self[:overall]
-    ov[:high_night] = {}
-    ov[:winners] = {}
-
-    self.each do |player_id, st|
-      next if player_id == :overall
-
-      ## calculate computed stats for player
-      st[:nights] += st[:dates].keys.length # if st[:nights] == 0
-      st[:high_night] = st[:dates].values.max
-      st[:gold_stars] = 1 if st[:nights] == 29
-      st[:warps_per_game]  = 1.0 * st[:warps] / st[:games]  rescue 0
-      st[:warps_per_night] = 1.0 * st[:warps] / st[:nights] rescue 0
-      st[:games_per_night] = 1.0 * st[:games] / st[:nights] rescue 0
-      st[:wins_per_night]  = 1.0 * st[:wins]  / st[:nights] rescue 0
-      st[:wins_per_game]   = 1.0 * st[:wins]  / st[:games]  rescue 0
-
-      ## accumulate overall stats
-      [:warps, :wins, :cfbs, :come_ons,
-       :wimps, :mystery_factors, :gold_stars]. each do |field|
-        ov[field] += st[field]
-      end
-      # nights won calculation
-      st[:dates].each do |date,warps|
-        ov[:dates][date] += warps
-        hnd = ov[:high_night][date] ||= {:players => [], :warps => 0}
-        if hnd[:warps] < warps
-          hnd[:players] = [player_id]
-          hnd[:warps] = warps
-        elsif hnd[:warps] == warps
-          hnd[:players].push(player_id)
-        end
-      end
-    end
-
-    ## update overall computed stats
-    st = self[:overall]
-    ov[:games] = ov[:wins]
-    ov[:nights] = ov[:dates].keys.length
-    ov[:nights] = 29 if ov[:nights] == 0 ## provide sane default
-    ov[:nights] = ov[:nights_real] if ov[:nights_real]
-    ov[:warps_per_game]  = 1.0 * ov[:warps] / ov[:games]  rescue 0
-    ov[:warps_per_night] = 1.0 * ov[:warps] / ov[:nights] rescue 0
-    ov[:games_per_night] = 1.0 * ov[:games] / ov[:nights] rescue 0
-    ov[:high_night].each do |date,h|
-      h[:players].each {|p| self[p][:nights_won] += 1}
-    end
-
-    ## determine per-stat winners
-    # fuck everyone but the top 50 OR those with 50+ warps
-    # the 51 below is not a bug
-    sorted_players = self.keys.sort{|a,b| self[b][:warps] <=> self[a][:warps]}
-    fifty_plus = self.keys.select{|p| self[p][:warps] >= 50}
-    eligible = (sorted_players[0..51] | fifty_plus).
-                  inject(Hash.new(false)) {|acc,p| acc.merge(p => true)}
-    [:warps, :games, :nights, :wins, :nights_won, :cfbs,
-     :come_ons, :wimps, :warps_per_game, :warps_per_night,
-     :games_per_night, :wins_per_game, :high_night].each do |field|
-      owf = ov[:winners][field] = {:players => [], :value => 0}
-      self.each do |player, st|
-        next if player == :overall
-        next unless eligible[player]
-        if st[field].to_f > owf[:value]
-          owf[:players] = [player]
-          owf[:value] = st[field]
-        elsif st[field] == owf[:value]
-          owf[:players].push(player)
-        end
-      end
-    end
-
-    ## mark per-stat winners
-    ov[:winners].each do |field, win|
-      win[:players].each do |player|
-        self[player][:winner][field] = true
-      end
-    end
-
-    self
-  end
-
-  def convert_player_ids_to_names!
-    ids = self.keys.select{|k| k.is_a?(Numeric)}
-    player_by_id = ids.inject({}) {|acc,id| acc.merge(id => Player.find(id))}
-    ids.each do |id|
-      p = player_by_id[id]
-      puts "no player for id #{id}" unless p
-      self[p.name] = self.delete(id) if p
-    end
-    self[:overall][:high_night].each do |date,h|
-      h[:players] = h[:players].map{|id| player_by_id[id]}
-    end
-    self
-  end
-
-  ## sort by warps
-  def to_a
-    a = []
-    a.push({:player => nil, :overall => true}.merge(self[:overall]))
-    (self.keys - [:overall]).
-      sort {|x,y| self[y][:warps] <=> self[x][:warps]}.
-      each_with_index {|k| a.push({:player => k}.merge(self[k]))}
-    a
-  end
-
-end
