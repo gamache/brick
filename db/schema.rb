6d4c38782edf5f86cabe6c892f491a8b9ee2ec96
pete gamache <pete@gamache.org>
2020-03-23 08:45:35 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2012_10_29_115333) do
   create_table "fudges", force: :cascade do |t|
     t.integer "player_id"
     t.integer "season"
-    t.string "notes", limit: 255
+    t.string "notes"
     t.integer "warps"
     t.integer "games"
     t.integer "nights"
@@ -27,27 +27,27 @@ ActiveRecord::Schema.define(version: 2012_10_29_115333) do
     t.integer "gold_stars"
     t.integer "nights_won"
     t.integer "high_night"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_fudges_on_player_id"
     t.index ["season"], name: "index_fudges_on_season"
   end
 
   create_table "locales", force: :cascade do |t|
-    t.string "place", limit: 255
-    t.string "phone", limit: 255
-    t.string "other", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "place"
+    t.string "phone"
+    t.string "other"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
-    t.string "name", limit: 255, null: false
-    t.string "display_name", limit: 255
-    t.string "password_salt", limit: 255
-    t.string "sha512_password", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name", null: false
+    t.string "display_name"
+    t.string "password_salt"
+    t.string "sha512_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_players_on_name", unique: true
   end
 
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 2012_10_29_115333) do
     t.boolean "cfb"
     t.boolean "come_on"
     t.boolean "mystery_factor"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_scores_on_player_id"
     t.index ["season"], name: "index_scores_on_season"
     t.index ["wimps"], name: "index_scores_on_wimps"
